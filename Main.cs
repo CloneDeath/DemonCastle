86fd7547be393b738ae0b4c891e97a576ce1a20c
Nicholas Rodine <halfofastaple@gmail.com>
2021-08-03 16:37:55 -0500

    Renamed project to DemonCastle with no space. Cleaned up Main.cs

--- Main.cs
+++ Main.cs
@@ -1,21 +1,8 @@
 using Godot;
-using System;
 
-public class Main : Node2D
-{
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+namespace DemonCastle {
+    public class Main : Node2D
     {
         
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
