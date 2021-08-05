a27ae71599e19e3a69e7ec4c42bc3f23ea0b05b5
Nicholas Rodine <halfofastaple@gmail.com>
2021-08-04 20:23:30 -0500

    Made System.IO priority over Godot in ProjectManager. Fixed crash when project dir doesn't exist. Also, renamed Games to Projects.

--- ProjectManager.cs
+++ ProjectManager.cs
@@ -1,43 +1,48 @@
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using DemonCastle.ProjectFiles;
-using Godot;
 using Newtonsoft.Json;
 
 namespace DemonCastle {
 	public class ProjectManager {
-		protected string GodotPath => "user://Games/";
-		protected string GlobalPath => ProjectSettings.GlobalizePath(GodotPath);
+		protected string GodotPath => "user://Projects/";
+		protected string GlobalPath => Godot.ProjectSettings.GlobalizePath(GodotPath);
 		
 		public void DownloadProjects() {
 			const string url = "https://github.com/CloneDeath/PixelPlatformerExample/archive/refs/heads/master.zip";
-			var dest = System.IO.Path.GetTempFileName();
+			var dest = Path.GetTempFileName();
 			using (var wc = new WebClient()) {
 				wc.DownloadFile(url, dest);
 			}
 
-			System.IO.Directory.Delete(GlobalPath, true);
+			if (Directory.Exists(GlobalPath)) {
+				Directory.Delete(GlobalPath, true);
+			}
+
 			ZipFile.ExtractToDirectory(dest, GlobalPath);
 		}
 
-		public List<ProjectFile> GetProjects() {
+		public IEnumerable<ProjectFile> GetProjects() {
 			var projectFiles = GetProjectFiles(GlobalPath);
-			return projectFiles.Select(AsProjectFile).ToList();
+			return projectFiles.Select(AsProjectFile);
 		}
 
 		protected ProjectFile AsProjectFile(string arg) {
-			var contents = System.IO.File.ReadAllText(arg);
+			var contents = File.ReadAllText(arg);
 			return JsonConvert.DeserializeObject<ProjectFile>(contents);
 		}
 
 		protected IEnumerable<string> GetProjectFiles(string path) {
-			var files = System.IO.Directory.GetFiles(path);
+			if (!Directory.Exists(path)) return new string[0];
+			
+			var files = Directory.GetFiles(path);
 			var relevantFiles = files.Where(f => f.ToLower().EndsWith(".dcp"));
 
 			// ReSharper disable once LoopCanBeConvertedToQuery
-			foreach (var subDir in System.IO.Directory.GetDirectories(path)) {
+			foreach (var subDir in Directory.GetDirectories(path)) {
 				relevantFiles = relevantFiles.Concat(GetProjectFiles(subDir));
 			}
 
