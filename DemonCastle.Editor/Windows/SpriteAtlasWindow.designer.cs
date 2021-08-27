545e104d98ff43362012f267654bd9ef27527dd8
Nicholas Rodine <halfofastaple@gmail.com>
2021-08-26 19:54:08 -0500

    Added split view to the atlas editor, with a preview of the sprite.

--- DemonCastle.Editor/Windows/SpriteAtlasWindow.designer.cs
+++ DemonCastle.Editor/Windows/SpriteAtlasWindow.designer.cs
@@ -4,18 +4,32 @@ using Godot;
 
 namespace DemonCastle.Editor.Windows {
 	public partial class SpriteAtlasWindow {
+		protected HSplitContainer SplitContainer { get; }
 		protected PropertyCollection PropertyCollection { get; }
+		protected ScrollContainer ScrollContainer { get; }
+		protected TextureRect TextureRect { get; }
+
 		public SpriteAtlasWindow(SpriteAtlasInfo spriteAtlasInfo) {
 			WindowTitle = spriteAtlasInfo.FileName;
 			RectSize = new Vector2(600, 300);
 
-			AddChild(PropertyCollection = new PropertyCollection {
+			AddChild(SplitContainer = new HSplitContainer {
+				AnchorRight = 1,
+				AnchorBottom = 1,
+				MarginTop = 5,
+				MarginBottom = -5,
+				MarginLeft = 5,
+				MarginRight = -5
+			});
+			
+			SplitContainer.AddChild(PropertyCollection = new PropertyCollection {
 				MarginTop = 5,
 				MarginRight = -5,
 				MarginBottom = -5,
 				MarginLeft = 5,
 				AnchorBottom = 1,
-				AnchorRight = 1
+				AnchorRight = 1,
+				RectMinSize = new Vector2(200, 0)
 			});
 			PropertyCollection.AddChild(new StringProperty {
 				PropertyName = "File",
@@ -24,6 +38,18 @@ namespace DemonCastle.Editor.Windows {
 			PropertyCollection.AddChild(new ColorProperty {
 				PropertyName = "Transparent Color",
 				PropertyValue = spriteAtlasInfo.TransparentColor
+			});
+			
+			SplitContainer.AddChild(ScrollContainer = new ScrollContainer {
+				AnchorRight = 1,
+				AnchorBottom = 1,
+				MarginLeft = 5,
+				MarginTop = 5,
+				MarginBottom = -5,
+				MarginRight = -5,
+			});
+			ScrollContainer.AddChild(TextureRect = new TextureRect {
+				Texture = spriteAtlasInfo.Texture
 			});
 		}
 	}
