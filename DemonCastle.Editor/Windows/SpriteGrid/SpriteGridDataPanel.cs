764bde2a1ab75377e9ac8d06f81ee4756db5a263
Nicholas Rodine <halfofastaple@gmail.com>
2021-09-14 18:11:42 -0500

    Added a preview to the sprite grid data panel.

--- DemonCastle.Editor/Windows/SpriteGrid/SpriteGridDataPanel.cs
+++ DemonCastle.Editor/Windows/SpriteGrid/SpriteGridDataPanel.cs
@@ -4,13 +4,33 @@ using Godot;
 
 namespace DemonCastle.Editor.Windows.SpriteGrid {
 	public class SpriteGridDataPanel : PanelContainer {
+		protected SpriteGridDataInfo SpriteData { get; }
 		protected PropertyCollection Properties { get; }
+		protected TextureRect Preview { get; }
+		protected AtlasTexture PreviewTexture { get; }
 		public SpriteGridDataPanel(SpriteGridDataInfo spriteData) {
+			SpriteData = spriteData;
+			
 			AddChild(Properties = new PropertyCollection());
 			Properties.AddString("Name", spriteData, x => x.Name);
 			Properties.AddInteger("X", spriteData, x => x.X);
 			Properties.AddInteger("Y", spriteData, x => x.Y);
 			Properties.AddBoolean("Flip Horizontal", spriteData, x => x.FlipHorizontal);
+			Properties.AddChild(Preview = new TextureRect {
+				Texture = PreviewTexture = new AtlasTexture {
+					Atlas = spriteData.Texture,
+					Region = spriteData.Region
+				},
+				FlipH = spriteData.FlipHorizontal
+			});
+		}
+
+		public override void _Process(float delta) {
+			base._Process(delta);
+
+			Preview.FlipH = SpriteData.FlipHorizontal;
+			PreviewTexture.Atlas = SpriteData.Texture;
+			PreviewTexture.Region = SpriteData.Region;
 		}
 	}
 }
