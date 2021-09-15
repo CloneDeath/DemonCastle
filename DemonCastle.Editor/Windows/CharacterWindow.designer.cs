7ef4afd59962d4b5b40465633727c7acb9f6bdf7
Nicholas Rodine <halfofastaple@gmail.com>
2021-09-15 16:35:03 -0500

    Moved around components in the character window to make space for animation editing.

--- DemonCastle.Editor/Windows/CharacterWindow.designer.cs
+++ DemonCastle.Editor/Windows/CharacterWindow.designer.cs
@@ -7,15 +7,14 @@ namespace DemonCastle.Editor.Windows {
 		protected PropertyCollection Properties { get; }
 		public CharacterWindow(CharacterInfo characterInfo) {
 			WindowTitle = $"Character - {characterInfo.FileName}";
-			RectSize = new Vector2(300, 300);
+			RectSize = new Vector2(600, 300);
 			RectMinSize = RectSize;
 
 			AddChild(Properties = new PropertyCollection {
 				MarginLeft = 5,
 				MarginTop = 5,
-				MarginRight = -5,
+				MarginRight = 205,
 				MarginBottom = -5,
-				AnchorRight = 1,
 				AnchorBottom = 1
 			});
 			Properties.AddString("Name", characterInfo, x => x.Name);
@@ -27,6 +26,21 @@ namespace DemonCastle.Editor.Windows {
 			Properties.AddString("Idle Animation", characterInfo, x => x.IdleAnimation);
 			Properties.AddString("Walk Animation", characterInfo, x => x.WalkAnimation);
 			Properties.AddString("Jump Animation", characterInfo, x => x.JumpAnimation);
+			
+			AddChild(new VSeparator {
+				Name = nameof(VSeparator),
+				RectPosition = new Vector2(210, 5),
+				MarginBottom = -5,
+				AnchorBottom = 1
+			});
+			
+			AddChild(new Panel {
+				RectPosition = new Vector2(215, 5),
+				MarginRight = -5,
+				MarginBottom = -5,
+				AnchorRight = 1,
+				AnchorBottom = 1
+			});
 		}
 	}
 }
