3f15024311f1ba087c2892f243dcf60c59c3182d
Nicholas Rodine <halfofastaple@gmail.com>
2021-09-30 16:11:11 -0500

    Added more details to each frame.

--- DemonCastle.Editor/Windows/Character/Animations/AnimationFramePanel.cs
+++ DemonCastle.Editor/Windows/Character/Animations/AnimationFramePanel.cs
@@ -2,20 +2,29 @@ using DemonCastle.ProjectFiles.Projects.Data;
 using Godot;
 
 namespace DemonCastle.Editor.Windows.Character.Animations {
-	public class AnimationFramePanel : Panel {
+	public class AnimationFramePanel : PanelContainer {
+		protected VBoxContainer Items { get; }
 		public AnimationFramePanel(FrameInfo frameInfo) {
 			RectMinSize = new Vector2(50, 50);
 			
-			AddChild(new Label {
+			AddChild(Items = new VBoxContainer {
+				AnchorRight = 1,
+				AnchorBottom = 1,
+				MarginRight = 0,
+				MarginBottom = 0,
+			});
+			
+			Items.AddChild(new Label {
 				Text = frameInfo.Index.ToString()
 			});
 
 			var texture = frameInfo.TextureRect;
-			AddChild(texture);
-			texture.AnchorRight = texture.AnchorBottom = 1;
-			texture.MarginRight = texture.MarginBottom = 0;
-			texture.MarginTop = 15;
+			Items.AddChild(texture);
 			texture.StretchMode = TextureRect.StretchModeEnum.KeepCentered;
+			
+			Items.AddChild(new Label {
+				Text = $"{frameInfo.Duration}s"
+			});
 		}
 	}
 }
