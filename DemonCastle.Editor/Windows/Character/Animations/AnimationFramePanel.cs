6a5c4b5ddb34a5f87f79e3e59ac184061cc27554
Nicholas Rodine <halfofastaple@gmail.com>
2021-10-01 16:10:36 -0500

    Added edit frame popup window.

--- DemonCastle.Editor/Windows/Character/Animations/AnimationFramePanel.cs
+++ DemonCastle.Editor/Windows/Character/Animations/AnimationFramePanel.cs
@@ -4,6 +4,10 @@ using Godot;
 namespace DemonCastle.Editor.Windows.Character.Animations {
 	public class AnimationFramePanel : PanelContainer {
 		protected VBoxContainer Items { get; }
+		protected Button EditButton { get; }
+		
+		protected EditFrameWindow Window { get; }
+		
 		public AnimationFramePanel(FrameInfo frameInfo) {
 			RectMinSize = new Vector2(50, 50);
 			
@@ -21,10 +25,21 @@ namespace DemonCastle.Editor.Windows.Character.Animations {
 			var texture = frameInfo.TextureRect;
 			Items.AddChild(texture);
 			texture.StretchMode = TextureRect.StretchModeEnum.KeepCentered;
-			
+
 			Items.AddChild(new Label {
 				Text = $"{frameInfo.Duration}s"
 			});
+			
+			Items.AddChild(EditButton = new Button {
+				Text = "Edit"
+			});
+			EditButton.Connect("pressed", this, nameof(OnEditButtonClicked));
+
+			AddChild(Window = new EditFrameWindow(frameInfo));
+		}
+
+		protected void OnEditButtonClicked() {
+			Window.PopupCentered();
 		}
 	}
 }
---
+++ DemonCastle.Editor/Windows/Character/Animations/EditFrameWindow.cs
@@ -0,0 +1,20 @@
+using DemonCastle.Editor.Windows.Properties;
+using DemonCastle.ProjectFiles.Projects.Data;
+using Godot;
+
+namespace DemonCastle.Editor.Windows.Character.Animations {
+	public class EditFrameWindow : WindowDialog {
+		protected PropertyCollection Properties { get; }
+		
+		public EditFrameWindow(FrameInfo frameInfo) {
+			Name = nameof(EditFrameWindow);
+			WindowTitle = "Edit Frame";
+			RectMinSize = new Vector2(200, 200);
+			PopupExclusive = true;
+
+			AddChild(Properties = new PropertyCollection());
+			
+			Properties.AddFloat("Duration", frameInfo, f => f.Duration);
+		}
+	}
+}
