85137cc70a53a5a096ab790cb424dd13e6bb8a5d
Nicholas Rodine <halfofastaple@gmail.com>
2023-08-27 07:40:31 -0500

    Only added tile collisions if they have any

--- DemonCastle.Game/GameTile.cs
+++ DemonCastle.Game/GameTile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using DemonCastle.ProjectFiles;
 using DemonCastle.ProjectFiles.Projects.Data.Levels;
 using Godot;
@@ -5,7 +6,7 @@ using Godot;
 namespace DemonCastle.Game; 
 
 public partial class GameTile : Node2D {
-	private StaticBody2D Body { get; }
+	private StaticBody2D? Body { get; }
 	
 	public GameTile(TileInfo tile, TileMapInfo tileMapInfo) {
 		Position = tileMapInfo.Position;
@@ -15,13 +16,15 @@ public partial class GameTile : Node2D {
 			RegionRect = tile.Region,
 			Centered = false
 		});
-		AddChild(Body = new StaticBody2D {
-			CollisionLayer = (uint)CollisionLayers.World
-		});
-		Body.AddChild(new CollisionShape2D {
-			Shape = new ConvexPolygonShape2D {
-				Points = tile.Collision
-			}
-		});
+		if (tile.Collision.Any()) {
+			AddChild(Body = new StaticBody2D {
+				CollisionLayer = (uint)CollisionLayers.World
+			});
+			Body.AddChild(new CollisionShape2D {
+				Shape = new ConvexPolygonShape2D {
+					Points = tile.Collision
+				}
+			});
+		}
 	}
 }
