04a57dd58d789c591cf6ab816d468f4103c5a497
Nicholas Rodine <halfofastaple@gmail.com>
2023-08-28 09:47:38 -0500

    adjusted colors and border

--- DemonCastle.Editor/Windows/Level/AreaCell.cs
+++ DemonCastle.Editor/Windows/Level/AreaCell.cs
@@ -7,14 +7,16 @@ public partial class AreaCell : Node2D {
 	public AreaCell(AreaInfo areaInfo) {
 		var levelInfo = areaInfo.LevelInfo;
 		Position = areaInfo.AreaPosition * levelInfo.AreaSize;
+
+		const int borderWidth = 2;
 		AddChild(new ColorRect {
-			Color = Colors.Blue,
+			Color = Colors.LightBlue,
 			Size = areaInfo.AreaSize * levelInfo.AreaSize
 		});
 		AddChild(new ColorRect {
-			Position = new Vector2(1, 1),
-			Color = Colors.LightBlue,
-			Size = areaInfo.AreaSize * levelInfo.AreaSize - new Vector2I(2, 2)
+			Position = new Vector2(1, 1) * borderWidth,
+			Color = Colors.Blue,
+			Size = areaInfo.AreaSize * levelInfo.AreaSize - new Vector2I(2, 2) * borderWidth
 		});
 	}
 }
