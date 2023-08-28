f30a8c5bdf3816446e0aa99446c446020bffbfb7
Nicholas Rodine <halfofastaple@gmail.com>
2023-08-28 05:00:44 -0500

    Added basic level editing added svg version of icon

--- DemonCastle.Editor/Windows/LevelWindow.designer.cs
+++ DemonCastle.Editor/Windows/LevelWindow.designer.cs
@@ -1,12 +1,28 @@
+using DemonCastle.Editor.Windows.Properties;
 using DemonCastle.ProjectFiles.Projects.Data.Levels;
 using Godot;
 
-namespace DemonCastle.Editor.Windows {
-	public partial class LevelWindow {
-		public LevelWindow(LevelInfo levelInfo) {
-			Title = $"Level - {levelInfo.FileName}";
-			Size = new Vector2I(300, 300);
-			MinSize = Size;
-		}
+namespace DemonCastle.Editor.Windows; 
+
+public partial class LevelWindow {
+	protected PropertyCollection Properties { get; }
+
+	public LevelWindow(LevelInfo levelInfo) {
+		Title = $"Level - {levelInfo.FileName}";
+		Size = new Vector2I(300, 300);
+		MinSize = Size;
+		
+		AddChild(Properties = new PropertyCollection {
+			OffsetLeft = 5,
+			OffsetTop = 5,
+			OffsetRight = 205,
+			OffsetBottom = -5,
+			AnchorBottom = 1
+		});
+		Properties.AddString("Name", levelInfo, x => x.Name);
+		Properties.AddInteger("Tile Width", levelInfo, x => x.TileWidth);
+		Properties.AddInteger("Tile Height", levelInfo, x => x.TileHeight);
+		Properties.AddInteger("Area Width", levelInfo, x => x.AreaWidth);
+		Properties.AddInteger("Area Height", levelInfo, x => x.AreaHeight);
 	}
 }
--- DemonCastle.ProjectFiles/Projects/Data/Levels/LevelInfo.cs
+++ DemonCastle.ProjectFiles/Projects/Data/Levels/LevelInfo.cs
@@ -3,35 +3,57 @@ using System.Linq;
 using DemonCastle.ProjectFiles.Projects.Resources;
 using Godot;
 
-namespace DemonCastle.ProjectFiles.Projects.Data.Levels {
-	public class LevelInfo : IListableInfo {
-		protected FileNavigator<LevelFile> File { get; }
-		protected LevelFile Level => File.Resource;
-		public string FileName => File.FileName;
+namespace DemonCastle.ProjectFiles.Projects.Data.Levels; 
 
-		public LevelTileSet TileSet { get; }
+public class LevelInfo : IListableInfo {
+	protected FileNavigator<LevelFile> File { get; }
+	protected LevelFile Level => File.Resource;
+	public string FileName => File.FileName;
 
-		public LevelInfo(FileNavigator<LevelFile> file) {
-			File = file;
-			TileSet = new LevelTileSet(Level, File);
-		}
-		
-		public string Name => Level.Name;
-		public int TileWidth => Level.TileWidth;
-		public int TileHeight => Level.TileHeight;
-		public Vector2I TileSize => new(Level.TileWidth, Level.TileHeight);
-		public Vector2I AreaSize => new(Level.AreaWidth, Level.AreaHeight);
+	public LevelTileSet TileSet { get; }
 
-		public IEnumerable<AreaInfo> Areas => Level.Areas.Select(area => new AreaInfo(area, this));
-
-		private AreaInfo GetAreaByName(string name) {
-			var area = Level.Areas.First(a => a.Name == name);
-			return new AreaInfo(area, this);
-		}
-
-		public Vector2 StartingLocation => TileSize * (
-			GetAreaByName(Level.StartingPosition.Area).TilePosition
-			+ new Vector2(Level.StartingPosition.X, Level.StartingPosition.Y)
-		) + new Vector2(TileWidth/2f, TileHeight);
+	public LevelInfo(FileNavigator<LevelFile> file) {
+		File = file;
+		TileSet = new LevelTileSet(Level, File);
 	}
+		
+	public string Name {
+		get => Level.Name;
+		set => Level.Name = value;
+	}
+
+	public int TileWidth {
+		get => Level.TileWidth;
+		set => Level.TileWidth = value;
+	}
+
+	public int TileHeight {
+		get => Level.TileHeight;
+		set => Level.TileHeight = value;
+	}
+
+	public int AreaWidth {
+		get => Level.AreaWidth;
+		set => Level.AreaWidth = value;
+	}
+
+	public int AreaHeight {
+		get => Level.AreaHeight;
+		set => Level.AreaHeight = value;
+	}
+
+	public Vector2I TileSize => new(Level.TileWidth, Level.TileHeight);
+	public Vector2I AreaSize => new(Level.AreaWidth, Level.AreaHeight);
+
+	public IEnumerable<AreaInfo> Areas => Level.Areas.Select(area => new AreaInfo(area, this));
+
+	private AreaInfo GetAreaByName(string name) {
+		var area = Level.Areas.First(a => a.Name == name);
+		return new AreaInfo(area, this);
+	}
+
+	public Vector2 StartingLocation => TileSize * (
+													  GetAreaByName(Level.StartingPosition.Area).TilePosition
+													  + new Vector2(Level.StartingPosition.X, Level.StartingPosition.Y)
+												  ) + new Vector2(TileWidth/2f, TileHeight);
 }
