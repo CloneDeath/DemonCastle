6da9fc62f6dcca7403a6f86f3d140b42fa974795
Nicholas Rodine <halfofastaple@gmail.com>
2023-10-22 07:00:04 -0500

    Reloaded a sprite atlas when the file changes

--- DemonCastle.Editor/Editors/SpriteAtlas/SpriteAtlasTextureView.cs
+++ DemonCastle.Editor/Editors/SpriteAtlas/SpriteAtlasTextureView.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using DemonCastle.Editor.Editors.Components;
 using DemonCastle.ProjectFiles.Projects.Data.Sprites;
@@ -6,9 +7,11 @@ using DemonCastle.ProjectFiles.Projects.Data.Sprites;
 namespace DemonCastle.Editor.Editors.SpriteAtlas;
 
 public partial class SpriteAtlasTextureView : TextureView {
+	private readonly SpriteAtlasInfo _spriteAtlasInfo;
 	private readonly List<SpriteAtlasArea> _areas = new();
 
 	public SpriteAtlasTextureView(SpriteAtlasInfo spriteAtlasInfo) {
+		_spriteAtlasInfo = spriteAtlasInfo;
 		Texture = spriteAtlasInfo.Texture;
 
 		foreach (var dataInfo in spriteAtlasInfo.SpriteData) {
@@ -17,6 +20,13 @@ public partial class SpriteAtlasTextureView : TextureView {
 			_areas.Add(area);
 			area.Selected += Area_OnSelected;
 		}
+
+		spriteAtlasInfo.PropertyChanged += SpriteAtlasInfo_OnPropertyChanged;
+	}
+
+	private void SpriteAtlasInfo_OnPropertyChanged(object? sender, PropertyChangedEventArgs e) {
+		if (e.PropertyName != "File") return;
+		Texture = _spriteAtlasInfo.Texture;
 	}
 
 	private void Area_OnSelected(SpriteAtlasArea selected) {
