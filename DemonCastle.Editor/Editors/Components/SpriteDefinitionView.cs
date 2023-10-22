bb8c57e5c102137ca48a4f95c4f4b9a9e6c16db7
Nicholas Rodine <halfofastaple@gmail.com>
2023-10-22 07:32:03 -0500

    Moved SpriteDefinitionTextureRect to a SpriteDefinitionView in the editor.

---
+++ DemonCastle.Editor/Editors/Components/SpriteDefinitionView.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel;
+using DemonCastle.ProjectFiles.Projects.Data.Sprites.SpriteDefinition;
+using Godot;
+
+namespace DemonCastle.Editor.Editors.Components;
+
+public partial class SpriteDefinitionView : CenterContainer {
+	private readonly ISpriteDefinition _definition;
+	private TextureRect Rect { get; set; }
+
+	public SpriteDefinitionView(ISpriteDefinition definition) {
+		_definition = definition;
+
+		AddChild(Rect = new TextureRect {
+			StretchMode = TextureRect.StretchModeEnum.KeepCentered
+		});
+		Reload();
+
+		if (definition is INotifyPropertyChanged definitionNotify) {
+			definitionNotify.PropertyChanged += DefinitionNotify_OnPropertyChanged;
+		}
+	}
+
+	private void DefinitionNotify_OnPropertyChanged(object? sender, PropertyChangedEventArgs e) {
+		Reload();
+	}
+
+	public void Reload() {
+		Rect.Texture = new AtlasTexture {
+			Atlas = _definition.Texture,
+			Region = _definition.Region,
+			FilterClip = true
+		};
+		Rect.FlipH = _definition.FlipHorizontal;
+	}
+}
