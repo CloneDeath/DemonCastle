9ed0ec8fc16c27d527a52323a9f05a8cd95441e0
Nicholas Rodine <halfofastaple@gmail.com>
2023-10-22 08:14:07 -0500

    Made it so that saving is done asynchronously

--- DemonCastle.ProjectFiles/Projects/Resources/FileNavigatorT.cs
+++ DemonCastle.ProjectFiles/Projects/Resources/FileNavigatorT.cs
@@ -1,22 +1,26 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace DemonCastle.ProjectFiles.Projects.Resources; 
+namespace DemonCastle.ProjectFiles.Projects.Resources;
 
 public class FileNavigator<T> : FileNavigator {
 	public T Resource { get; }
-		
+	private Task _saveTask = Task.CompletedTask;
+
 	public FileNavigator(string filePath) : this(filePath, new ProjectResources()) { }
-	public FileNavigator(string filePath, ProjectResources resources) 
+	public FileNavigator(string filePath, ProjectResources resources)
 		: base(filePath, resources) {
 		var fileContents = File.ReadAllText(filePath);
 		Resource = JsonConvert.DeserializeObject<T>(fileContents)
 				   ?? throw new NullReferenceException();
 	}
 
+	public bool Saving => _saveTask.IsCompleted;
+
 	public void Save() {
 		var contents = JsonConvert.SerializeObject(Resource, Formatting.Indented);
-		File.WriteAllText(FilePath, contents);
+		_saveTask = _saveTask.ContinueWith((_task) => File.WriteAllTextAsync(FilePath, contents));
 	}
 }
