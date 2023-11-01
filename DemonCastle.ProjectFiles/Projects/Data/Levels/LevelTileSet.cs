824572b243d3aac0ed5d6af1a4c4abd082020d0c
Nicholas Rodine <halfofastaple@gmail.com>
2023-11-01 13:43:59 -0500

    Fixed it so that tileinfo is persistant.

--- DemonCastle.ProjectFiles/Projects/Data/Levels/LevelTileSet.cs
+++ DemonCastle.ProjectFiles/Projects/Data/Levels/LevelTileSet.cs
@@ -6,30 +6,42 @@ using DemonCastle.ProjectFiles.Projects.Resources;
 namespace DemonCastle.ProjectFiles.Projects.Data.Levels;
 
 public class LevelTileSet {
+	private readonly List<TileInfo> _tiles;
+
 	protected LevelFile Level { get; }
 	protected FileNavigator<LevelFile> File { get; }
 
-	public IEnumerable<TileInfo> Tiles => Level.Tiles.Select(t => new TileInfo(File, t));
+	public IEnumerable<TileInfo> Tiles => _tiles;
 
 	public LevelTileSet(LevelFile level, FileNavigator<LevelFile> file) {
 		File = file;
 		Level = level;
+		_tiles = Level.Tiles.Select(t => new TileInfo(File, t)).ToList();
 	}
 
 	public TileInfo GetTileInfo(Guid tileId) {
-		return Tiles.First(t => t.Id == tileId);
+		return _tiles.First(t => t.Id == tileId);
 	}
 
 	public TileInfo CreateTile() {
 		var tileData = new TileData();
 		Level.Tiles.Add(tileData);
 		File.Save();
-		return new TileInfo(File, tileData);
+
+		var tileInfo = new TileInfo(File, tileData);
+		_tiles.Add(tileInfo);
+		return tileInfo;
 	}
 
 	public void DeleteTile(TileInfo tileInfo) {
-		var index = Level.Tiles.FindIndex(t => t.Name == tileInfo.Name);
-		if (index < 0) return;
-		Level.Tiles.RemoveAt(index);
+		var index = Level.Tiles.FindIndex(t => t.Id == tileInfo.Id);
+		if (index >= 0) {
+			Level.Tiles.RemoveAt(index);
+		}
+
+		var infoIndex = _tiles.FindIndex(t => t.Id == tileInfo.Id);
+		if (infoIndex >= 0) {
+			_tiles.RemoveAt(infoIndex);
+		}
 	}
 }
