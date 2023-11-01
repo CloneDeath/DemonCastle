e67bf2822dfc11162992f7a9ba5f1f91ac765829
Nicholas Rodine <halfofastaple@gmail.com>
2023-11-01 13:37:35 -0500

    working on getting tile view to auto-load when you change a property

--- DemonCastle.Editor/Editors/Level/Area/AreaTiles/TileView.cs
+++ DemonCastle.Editor/Editors/Level/Area/AreaTiles/TileView.cs
@@ -1,17 +1,34 @@
+using System.ComponentModel;
+using DemonCastle.Editor.Editors.Components;
 using DemonCastle.ProjectFiles.Projects.Data.Levels;
-using Godot;
 
 namespace DemonCastle.Editor.Editors.Level.Area.AreaTiles;
 
-public partial class TileView : TextureRect {
-	public TileView(TileMapInfo tile) {
+public partial class TileView : SpriteDefinitionView {
+	protected TileMapInfo Tile { get; }
+
+	public TileView(TileMapInfo tile) : base(tile.Sprite) {
+		Tile = tile;
+
+		Name = nameof(TileView);
 		Position = tile.Position.ToPixelPositionInArea();
-		Texture = new AtlasTexture {
-			Atlas = tile.Texture,
-			Region = tile.Region,
-			FilterClip = true
-		};
-		FlipH = tile.FlipHorizontal;
-		Scale = tile.TileScale * tile.Span / tile.Region.Size ;
+		LoadScale();
+
+		tile.PropertyChanged += Tile_OnPropertyChanged;
+	}
+
+	private void Tile_OnPropertyChanged(object? sender, PropertyChangedEventArgs e) {
+		switch (e.PropertyName) {
+			case nameof(Tile.Sprite):
+				Load(Tile.Sprite);
+				break;
+			case nameof(Tile.Span):
+				LoadScale();
+				break;
+		}
+	}
+
+	private void LoadScale() {
+		Scale = Tile.TileScale * Tile.Span / Tile.Region.Size;
 	}
 }
--- DemonCastle.ProjectFiles/Projects/Data/Levels/TileMapInfo.cs
+++ DemonCastle.ProjectFiles/Projects/Data/Levels/TileMapInfo.cs
@@ -1,28 +1,58 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using DemonCastle.ProjectFiles.Locations;
+using DemonCastle.ProjectFiles.Projects.Data.Sprites.SpriteDefinition;
 using Godot;
 
 namespace DemonCastle.ProjectFiles.Projects.Data.Levels;
 
-public class TileMapInfo {
+public class TileMapInfo : INotifyPropertyChanged {
 	protected TileMapData TileMapData { get; }
 	protected AreaInfo AreaInfo { get; }
+
+	public TileInfo Tile => AreaInfo.GetTileInfo(TileId);
+	public ISpriteDefinition Sprite => Tile.Sprite;
 
 	public TileMapInfo(TileMapData tileMapData, AreaInfo areaInfo) {
 		TileMapData = tileMapData;
 		AreaInfo = areaInfo;
+		Tile.PropertyChanged += Tile_OnPropertyChanged;
+	}
+
+	private void Tile_OnPropertyChanged(object? sender, PropertyChangedEventArgs e) {
+		switch (e.PropertyName) {
+			case nameof(Tile.Sprite):
+				OnPropertyChanged(nameof(Sprite));
+				break;
+			case nameof(Tile.Span):
+				OnPropertyChanged(nameof(Span));
+				break;
+		}
 	}
 
 	protected LevelTileSet TileSet => AreaInfo.LevelTileSet;
 	protected Vector2I TileIndex => new(TileMapData.X, TileMapData.Y);
 
-	public TileInfo Tile => AreaInfo.GetTileInfo(TileId);
-
 	public Vector2I TileScale => AreaInfo.TileSize;
 	public TilePosition Position => new(TileIndex, AreaInfo.PositionOfArea, TileScale);
 	public Guid TileId => TileMapData.TileId;
-	public Texture2D Texture => Tile.Texture;
-	public Rect2 Region => Tile.Region;
-	public bool FlipHorizontal => Tile.FlipHorizontal;
 	public Vector2I Span => Tile.Span;
+	public Rect2 Region => Sprite.Region;
+
+	#region INotifyPropertyChanged
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) {
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+
+	protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null) {
+		if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+		field = value;
+		OnPropertyChanged(propertyName);
+		return true;
+	}
+	#endregion
 }
