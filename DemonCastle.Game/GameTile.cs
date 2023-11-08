70cb605a855144dd4692691a31aaa6f467f7745a
Nicholas Rodine <halfofastaple@gmail.com>
2023-11-08 06:01:29 -0500

    Added a tile collision shape if it contains stairs.

--- DemonCastle.Game/GameTile.cs
+++ DemonCastle.Game/GameTile.cs
@@ -6,9 +6,15 @@ using Godot;
 namespace DemonCastle.Game;
 
 public partial class GameTile : Node2D {
-	private StaticBody2D? Body { get; }
+	private readonly TileInfo _tile;
+	private StaticBody2D? Body { get; set; }
+	private Area2D? Stairs { get; set; }
 
 	public GameTile(TileInfo tile) {
+		_tile = tile;
+
+		Name = nameof(GameTile);
+
 		AddChild(new Sprite2D {
 			Texture = tile.Texture,
 			RegionEnabled = true,
@@ -17,13 +23,31 @@ public partial class GameTile : Node2D {
 			Scale = tile.Span * tile.TileSize / tile.Region.Size
 		});
 
-		if (!tile.Collision.Any()) return;
+		SetupCollisions();
+		SetupStairs();
+	}
+
+	private void SetupCollisions() {
+		if (!_tile.Collision.Any()) return;
 		AddChild(Body = new StaticBody2D {
 			CollisionLayer = (uint)CollisionLayers.World
 		});
 		Body.AddChild(new CollisionShape2D {
 			Shape = new ConvexPolygonShape2D {
-				Points = tile.Collision.Select(v => v * tile.TileSize * tile.Span).ToArray()
+				Points = _tile.Collision.Select(v => v * _tile.TileSize * _tile.Span).ToArray()
+			}
+		});
+	}
+
+	private void SetupStairs() {
+		if (_tile.Stairs == null) return;
+
+		var size = _tile.TileSize * _tile.Span;
+		AddChild(Stairs = new Area2D());
+		Stairs.AddChild(new CollisionShape2D {
+			Position = size/2,
+			Shape = new RectangleShape2D {
+				Size = size
 			}
 		});
 	}
