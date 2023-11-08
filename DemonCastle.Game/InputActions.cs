d6a8b7df3fd111739d21f90d940d3775d2047f9b
Nicholas Rodine <halfofastaple@gmail.com>
2023-11-08 05:48:49 -0500

    Added up and down controls

--- DemonCastle.Game/InputActions.cs
+++ DemonCastle.Game/InputActions.cs
@@ -1,26 +1,30 @@
 using System.Linq;
 using Godot;
 
-namespace DemonCastle.Game; 
+namespace DemonCastle.Game;
 
 public static class InputActions {
 	public const string PlayerMoveLeft = nameof(PlayerMoveLeft);
 	public const string PlayerMoveRight = nameof(PlayerMoveRight);
+	public const string PlayerMoveUp = nameof(PlayerMoveUp);
+	public const string PlayerMoveDown = nameof(PlayerMoveDown);
 	public const string PlayerJump = nameof(PlayerJump);
 
 	public const string EditorSubmit = nameof(EditorSubmit);
 	public const string EditorClick = nameof(EditorClick);
 	public const string EditorRightClick = nameof(EditorRightClick);
 	public const string EditorRename = nameof(EditorRename);
-		
+
 	public static void RegisterActions() {
 		RegisterAction(PlayerMoveLeft, Key.Left, Key.A);
 		RegisterAction(PlayerMoveRight, Key.Right, Key.D);
-		RegisterAction(PlayerJump, Key.Up, Key.W);
+		RegisterAction(PlayerJump, Key.Space);
 		RegisterAction(EditorSubmit, Key.Enter, Key.KpEnter);
 		RegisterAction(EditorClick, MouseButton.Left);
 		RegisterAction(EditorRightClick, MouseButton.Right);
 		RegisterAction(EditorRename, Key.F2);
+		RegisterAction(PlayerMoveUp, Key.Up, Key.W);
+		RegisterAction(PlayerMoveDown, Key.Down, Key.S);
 	}
 
 	private static void RegisterAction(string actionName, params Key[] keyList) {
@@ -28,7 +32,7 @@ public static class InputActions {
 			Keycode = key
 		}).ToArray());
 	}
-		
+
 	// ReSharper disable once UnusedMember.Local
 	private static void RegisterAction(string actionName, params MouseButton[] mouseButtons) {
 		RegisterAction(actionName, mouseButtons.Select(button => (InputEvent)new InputEventMouseButton {
@@ -40,7 +44,7 @@ public static class InputActions {
 		if (InputMap.HasAction(actionName)) return;
 		InputMap.AddAction(actionName);
 		foreach (var inputEvent in events) {
-			InputMap.ActionAddEvent(actionName, inputEvent);	
+			InputMap.ActionAddEvent(actionName, inputEvent);
 		}
 	}
 }
