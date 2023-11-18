ff865a26e7ebf233a8ae7156668e08697e84b214
Nicholas Rodine <halfofastaple@gmail.com>
2023-11-18 06:42:43 -0500

    Consolidated the anim collection edit control

--- DemonCastle.Editor/Editors/Character/Animations/AnimationCollectionEdit.cs
+++ DemonCastle.Editor/Editors/Character/Animations/AnimationCollectionEdit.cs
@@ -4,11 +4,36 @@ using DemonCastle.ProjectFiles;
 using DemonCastle.ProjectFiles.Projects.Data;
 using Godot;
 
-namespace DemonCastle.Editor.Editors.Character.Animations; 
+namespace DemonCastle.Editor.Editors.Character.Animations;
 
 public partial class AnimationCollectionEdit : VBoxContainer {
-	public event Action<AnimationInfo>? AnimationSelected; 
-		
+	protected AnimationInfoCollection Animations { get; }
+	protected AnimationItemList AnimationItems { get; }
+	protected Button AddButton { get; }
+	protected Button RemoveButton { get; }
+
+	public event Action<AnimationInfo>? AnimationSelected;
+
+	public AnimationCollectionEdit(AnimationInfoCollection animations) {
+		Animations = animations;
+
+		AddChild(AddButton = new Button {
+			Text = "Add"
+		});
+		AddButton.Pressed += OnAddPressed;
+
+		AddChild(AnimationItems = new AnimationItemList {
+			SizeFlagsVertical = SizeFlags.ExpandFill
+		});
+		AnimationItems.ItemSelected += OnAnimationSelected;
+		AnimationItems.AddAnimations(animations);
+
+		AddChild(RemoveButton = new Button {
+			Text = "Remove"
+		});
+		RemoveButton.Pressed += OnRemovePressed;
+	}
+
 	public override void _Process(double delta) {
 		base._Process(delta);
 
