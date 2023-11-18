ee499fb9decb13a3106b40caabfe650b04a7fda4
Nicholas Rodine <halfofastaple@gmail.com>
2023-11-18 06:23:26 -0500

    made it so the animation dropdowns are reactive to changes in the animations list

--- DemonCastle.Editor/Editors/Properties/AnimationNameProperty.cs
+++ DemonCastle.Editor/Editors/Properties/AnimationNameProperty.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using DemonCastle.Editor.Properties;
 using DemonCastle.ProjectFiles.Projects.Data;
@@ -7,34 +8,52 @@ using Godot;
 namespace DemonCastle.Editor.Editors.Properties;
 
 public partial class AnimationNameProperty : BaseProperty {
-	private readonly List<AnimationInfo> _options;
+	private readonly IEnumerable<AnimationInfo> _options;
+
+	private List<AnimationInfo> Options => _options.ToList();
+
 	protected IPropertyBinding<string> Binding { get; }
 	protected OptionButton OptionButton { get; }
 
 	public string PropertyValue {
-		get => OptionButton.Selected < 0 ? string.Empty : _options[OptionButton.Selected].Name;
+		get => OptionButton.Selected < 0 ? string.Empty : Options[OptionButton.Selected].Name;
 		set {
 			var option = _options.FirstOrDefault(o => o.Name == value);
-			OptionButton.Selected = option == null ? -1 : _options.IndexOf(option);
+			OptionButton.Selected = option == null ? -1 : Options.IndexOf(option);
 		}
 	}
 
 	public AnimationNameProperty(IPropertyBinding<string> binding, IEnumerable<AnimationInfo> options) {
-		_options = options.ToList();
+		_options = options;
 		Name = nameof(AnimationNameProperty);
 		Binding = binding;
+
+		if (options is INotifyCollectionChanged collectionChanged) {
+			collectionChanged.CollectionChanged += Options_OnCollectionChanged;
+		}
 
 		AddChild(OptionButton = new OptionButton {
 			CustomMinimumSize = new Vector2(20, 20),
 			SizeFlagsHorizontal = SizeFlags.ExpandFill
 		});
-		for (var i = 0; i < _options.Count; i++) {
-			var option = _options[i];
-			OptionButton.AddItem(option.Name, i);
-		}
+		ReloadOptions();
 		PropertyValue = binding.Get();
 
 		OptionButton.ItemSelected += OnItemSelected;
+	}
+
+	private void ReloadOptions() {
+		OptionButton.Clear();
+		for (var i = 0; i < Options.Count; i++) {
+			var option = Options[i];
+			OptionButton.AddItem(option.Name, i);
+		}
+	}
+
+	private void Options_OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e) {
+		var selected = OptionButton.Selected;
+		ReloadOptions();
+		OptionButton.Selected = selected;
 	}
 
 	public override void _EnterTree() {
@@ -52,7 +71,7 @@ public partial class AnimationNameProperty : BaseProperty {
 	}
 
 	private void OnItemSelected(long index) {
-		Binding.Set(index < 0 ? string.Empty : _options[(int)index].Name);
+		Binding.Set(index < 0 ? string.Empty : Options[(int)index].Name);
 	}
 
 	public override void Enable() {
--- DemonCastle.ProjectFiles/Projects/Data/AnimationInfoCollection.cs
+++ DemonCastle.ProjectFiles/Projects/Data/AnimationInfoCollection.cs
@@ -1,39 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using DemonCastle.ProjectFiles.Projects.Resources;
 
-namespace DemonCastle.ProjectFiles.Projects.Data; 
+namespace DemonCastle.ProjectFiles.Projects.Data;
 
-public class AnimationInfoCollection : IEnumerable<AnimationInfo> {
+public class AnimationInfoCollection : IEnumerable<AnimationInfo>, INotifyCollectionChanged {
 	protected FileNavigator<CharacterFile> File { get; }
 	protected List<AnimationData> FileAnimations { get; }
-	protected List<AnimationInfo> Animations { get; }
+	protected ObservableCollection<AnimationInfo> Animations { get; }
 
 	public AnimationInfoCollection(FileNavigator<CharacterFile> file, List<AnimationData> animations) {
 		File = file;
 		FileAnimations = animations;
-		Animations = animations.Select(data => new AnimationInfo(file, data)).ToList();
+		Animations = new ObservableCollection<AnimationInfo>(animations.Select(data => new AnimationInfo(file, data)).ToList());
+		Animations.CollectionChanged += Animations_OnCollectionChanged;
 	}
 
 	public AnimationInfo Add(AnimationData animationData) {
 		FileAnimations.Add(animationData);
+		Save();
 		var animInfo = new AnimationInfo(File, animationData);
 		Animations.Add(animInfo);
-		Save();
 		return animInfo;
 	}
 
-	public IEnumerator<AnimationInfo> GetEnumerator() => Animations.GetEnumerator();
-	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
 	public void RemoveAt(int animationIndex) {
-		Animations.RemoveAt(animationIndex);
 		FileAnimations.RemoveAt(animationIndex);
 		Save();
+		Animations.RemoveAt(animationIndex);
 	}
 
 	protected void Save() => File.Save();
 
 	public AnimationInfo this[int index] => Animations[index];
+
+	#region IEnumerable
+	public IEnumerator<AnimationInfo> GetEnumerator() => Animations.GetEnumerator();
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+	#endregion
+
+	#region INotifyCollectionChanged
+	public event NotifyCollectionChangedEventHandler? CollectionChanged;
+
+	private void Animations_OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e) {
+		CollectionChanged?.Invoke(this, e);
+	}
+	#endregion
 }
