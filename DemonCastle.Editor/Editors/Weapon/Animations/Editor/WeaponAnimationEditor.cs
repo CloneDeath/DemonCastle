83077be3b45482dbbb1fc628176f33aca1ee67d4
Nicholas Rodine <halfofastaple@gmail.com>
2023-11-19 04:22:52 -0500

    Added label to the editor.

--- DemonCastle.Editor/Editors/Weapon/Animations/Editor/WeaponAnimationEditor.cs
+++ DemonCastle.Editor/Editors/Weapon/Animations/Editor/WeaponAnimationEditor.cs
@@ -3,5 +3,7 @@ using Godot;
 namespace DemonCastle.Editor.Editors.Weapon.Animations.Editor;
 
 public partial class WeaponAnimationEditor : VBoxContainer {
-
+	public WeaponAnimationEditor() {
+		AddChild(new Label {Text = "Weapon Animation Editor"});
+	}
 }
