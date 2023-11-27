d3e91e5d65ec93cd0be98567ca90df8a9ef768b7
Nicholas Rodine <halfofastaple@gmail.com>
2023-11-27 06:45:12 -0500

    Fleshed out monster editor a little

--- DemonCastle.Editor/Editors/Monster/MonsterEditor.cs
+++ DemonCastle.Editor/Editors/Monster/MonsterEditor.cs
@@ -7,9 +7,21 @@ public partial class MonsterEditor : BaseEditor {
 	public override Texture2D TabIcon => EditorFileType.Monster.Icon;
 	public override string TabText { get; }
 
+	protected HSplitContainer SplitContainer { get; }
+	protected TabContainer RightArea { get; }
+
 	public MonsterEditor(MonsterInfo monster) {
 		TabText = monster.FileName;
 
-		AddChild(new MonsterDetails(monster));
+		AddChild(SplitContainer = new HSplitContainer());
+		SplitContainer.SetAnchorsPreset(LayoutPreset.FullRect);
+
+		SplitContainer.AddChild(new MonsterDetails(monster));
+		SplitContainer.AddChild(RightArea = new TabContainer());
+
+		RightArea.AddChild(new Control());
+		RightArea.SetTabTitle(0, "Animations");
+		RightArea.AddChild(new Control());
+		RightArea.SetTabTitle(1, "States");
 	}
 }
