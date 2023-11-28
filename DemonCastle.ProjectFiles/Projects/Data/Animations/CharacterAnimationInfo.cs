ed410458c31e01d88e078e2148c9711f7e3bb19c
Nicholas Rodine <halfofastaple@gmail.com>
2023-11-28 09:18:16 -0500

    Removed unused using.

--- DemonCastle.ProjectFiles/Projects/Data/Animations/CharacterAnimationInfo.cs
+++ DemonCastle.ProjectFiles/Projects/Data/Animations/CharacterAnimationInfo.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using DemonCastle.ProjectFiles.Files;
 using DemonCastle.ProjectFiles.Files.Animations;
