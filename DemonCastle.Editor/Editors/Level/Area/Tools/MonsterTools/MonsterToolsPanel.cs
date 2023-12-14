04b622aa2f0410e800de98cdbb2194ccc9d24b19
Nicholas Rodine <halfofastaple@gmail.com>
2023-12-14 16:26:25 -0500

    Added preview for enemies in the level monster list

--- DemonCastle.Editor/Editors/Level/Area/Tools/MonsterTools/MonsterToolsPanel.cs
+++ DemonCastle.Editor/Editors/Level/Area/Tools/MonsterTools/MonsterToolsPanel.cs
@@ -1,4 +1,7 @@
+using System.Linq;
+using DemonCastle.Editor.Editors.Components;
 using DemonCastle.ProjectFiles.Projects.Data;
+using DemonCastle.ProjectFiles.Projects.Data.Sprites;
 using Godot;
 
 namespace DemonCastle.Editor.Editors.Level.Area.Tools.MonsterTools;
@@ -9,6 +12,9 @@ public partial class MonsterToolsPanel : VBoxContainer {
 
 		AddChild(new Label { Text = "Monster Tools"});
 		foreach (var monster in project.Monsters) {
+			var state = monster.States.FirstOrDefault(s => s.Id == monster.InitialState);
+			var animation = monster.Animations.FirstOrDefault(a => a.Id == state?.Animation);
+			AddChild(new SpriteDefinitionView( animation?.Frames.First().SpriteDefinition ?? new NullSpriteDefinition()));
 			AddChild(new Label { Text = monster.Name});
 		}
 	}
