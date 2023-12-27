26c49991b00e19b2bb0f1bb4be5a6ad0c26f90b4
Nicholas Rodine <halfofastaple@gmail.com>
2023-12-27 10:06:41 -0500

    Sketched out UI for scene editor.

--- DemonCastle.Editor/Editors/Scene/SceneEditor.cs
+++ DemonCastle.Editor/Editors/Scene/SceneEditor.cs
@@ -8,17 +8,36 @@ public partial class SceneEditor : BaseEditor {
 	public override Texture2D TabIcon => EditorFileType.Scene.Icon;
 	public override string TabText { get; }
 
+	private HSplitContainer Split { get; }
+
 	private VBoxContainer Left { get; }
+	private HSplitContainer Right { get; }
 
 	public SceneEditor(SceneInfo scene) {
 		TabText = scene.FileName;
 
-		AddChild(Left = new VBoxContainer());
-		Left.SetAnchorsAndOffsetsPreset(LayoutPreset.FullRect, margin: 5);
+		AddChild(Split = new HSplitContainer());
+		Split.SetAnchorsAndOffsetsPreset(LayoutPreset.FullRect, margin: 5);
 
-		Left.AddChild(new SceneDetails(scene));
-		Left.AddChild(new ElementList(scene.Elements) {
-			SizeFlagsVertical = SizeFlags.ExpandFill
+		Split.AddChild(Left = new VBoxContainer {
+			CustomMinimumSize = new Vector2(300, 300)
 		});
+		{
+			Left.AddChild(new SceneDetails(scene));
+			Left.AddChild(new ElementList(scene.Elements) {
+				SizeFlagsVertical = SizeFlags.ExpandFill
+			});
+		}
+
+		Split.AddChild(Right = new HSplitContainer());
+		{
+			Right.AddChild(new VBoxContainer {
+				CustomMinimumSize = new Vector2(300, 300)
+			});
+			Right.AddChild(new ColorRect {
+				Color = Colors.Red,
+				CustomMinimumSize = new Vector2(300, 300)
+			});
+		}
 	}
 }
