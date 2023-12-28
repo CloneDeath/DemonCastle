54e293b684035c00b0b1171b347a2eeb60c0208a
Nicholas Rodine <halfofastaple@gmail.com>
2023-12-28 12:25:08 -0500

    Playing with UI, and what the event editor might look like.

--- DemonCastle.Editor/Editors/Scene/Events/Editor/SceneEventActionEditor.cs
+++ DemonCastle.Editor/Editors/Scene/Events/Editor/SceneEventActionEditor.cs
@@ -3,9 +3,22 @@ using Godot;
 
 namespace DemonCastle.Editor.Editors.Scene.Events.Editor;
 
-public partial class SceneEventActionEditor : VBoxContainer {
+public partial class SceneEventActionEditor : HFlowContainer {
 	public SceneEventActionEditor(SceneEventActionInfo then) {
 		Name = nameof(SceneEventActionEditor);
 		AddChild(new Label{Text = "Then"});
+
+
+		OptionButton optionButton;
+		AddChild(optionButton = new OptionButton());
+		optionButton.AddItem("Scene");
+
+		OptionButton optionButton2;
+		AddChild(optionButton2 = new OptionButton());
+		optionButton2.AddItem("Set");
+		optionButton2.AddItem("Push");
+		optionButton2.AddItem("Pop");
+
+		AddChild(new LineEdit {Text = "other_scene.dcs"});
 	}
 }
--- DemonCastle.Editor/Editors/Scene/Events/Editor/SceneEventConditionEditor.cs
+++ DemonCastle.Editor/Editors/Scene/Events/Editor/SceneEventConditionEditor.cs
@@ -3,9 +3,36 @@ using Godot;
 
 namespace DemonCastle.Editor.Editors.Scene.Events.Editor;
 
-public partial class SceneEventConditionEditor : VBoxContainer {
+public partial class SceneEventConditionEditor : HFlowContainer {
 	public SceneEventConditionEditor(SceneEventConditionInfo when) {
 		Name = nameof(SceneEventConditionEditor);
 		AddChild(new Label{Text = "When"});
+
+		OptionButton optionButton;
+		AddChild(optionButton = new OptionButton());
+		optionButton.AddItem("Button");
+
+		AddChild(new Label{Text = "is"});
+		OptionButton optionButton2;
+		AddChild(optionButton2 = new OptionButton());
+		optionButton2.AddItem("Pressed");
+		optionButton2.AddItem("Released");
+		optionButton2.AddItem("Down");
+		optionButton2.AddItem("Up");
+
+		AddChild(new Label{Text = "("});
+		OptionButton optionButton3;
+		AddChild(optionButton3 = new OptionButton());
+		optionButton3.AddItem("Up");
+		optionButton3.AddItem("Down");
+		optionButton3.AddItem("Left");
+		optionButton3.AddItem("Right");
+		optionButton3.AddItem("A");
+		optionButton3.AddItem("B");
+		optionButton3.AddItem("X");
+		optionButton3.AddItem("Y");
+		optionButton3.AddItem("Start");
+		optionButton3.AddItem("Select");
+		AddChild(new Label{Text = ")"});
 	}
 }
