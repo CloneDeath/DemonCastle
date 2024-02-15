61d2e9d5e8e0051412c8b83c2156939caf97ee02
Nicholas Rodine <halfofastaple@gmail.com>
2024-02-15 05:18:40 -0500

    Added button groups to the ToolStrip

--- DemonCastle.Editor/Editors/Level/Area/Tools/TileTools/ToolStrip.cs
+++ DemonCastle.Editor/Editors/Level/Area/Tools/TileTools/ToolStrip.cs
@@ -3,18 +3,32 @@ using Godot;
 
 namespace DemonCastle.Editor.Editors.Level.Area.Tools.TileTools;
 
+public enum TileTool {
+	Brush,
+	Fill
+}
+
 public partial class ToolStrip : VBoxContainer {
+	public TileTool SelectedTool { get; private set; } = TileTool.Brush;
+
+	protected Button BrushTool { get; }
+	protected Button FillTool { get; }
+
 	public ToolStrip() {
 		Name = nameof(ToolStrip);
 
-		AddChild(new Button {
+		var group = new ButtonGroup();
+		AddChild(BrushTool = new Button {
 			Icon = IconTextures.BrushToolIcon,
 			ToggleMode = true,
+			ButtonGroup = group,
 			ButtonPressed = true
 		});
-		AddChild(new Button {
+
+		AddChild(FillTool = new Button {
 			Icon = IconTextures.FillToolIcon,
-			ToggleMode = true
+			ToggleMode = true,
+			ButtonGroup = group
 		});
 	}
 }
