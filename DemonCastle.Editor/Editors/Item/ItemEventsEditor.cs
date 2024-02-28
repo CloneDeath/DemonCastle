717e4084b8b872fab532b30c6033c4449a2de601
Nicholas Rodine <halfofastaple@gmail.com>
2024-02-28 05:50:53 -0500

    Added "On Pickup" text to the item event editor.

--- DemonCastle.Editor/Editors/Item/ItemEventsEditor.cs
+++ DemonCastle.Editor/Editors/Item/ItemEventsEditor.cs
@@ -1,5 +1,6 @@
 using DemonCastle.Editor.Editors.Components.Actions;
 using DemonCastle.ProjectFiles.Projects.Data;
+using Godot;
 
 namespace DemonCastle.Editor.Editors.Item;
 
@@ -7,6 +8,10 @@ public partial class ItemEventsEditor : ActionCollectionEditor<ItemActionInfo> {
 	public ItemEventsEditor(IEnumerableInfo<ItemActionInfo> actionSet) {
 		Name = nameof(ItemEventsEditor);
 		_actionSet = actionSet;
+
+		Label onPickup;
+		AddChild(onPickup = new Label{Text = "On Pickup"});
+		MoveChild(onPickup, 0);
 	}
 
 	protected override void AddAction(ItemActionInfo action) {
