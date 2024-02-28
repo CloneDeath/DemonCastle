1d3582184cc4b13464fa1a1383d391331fb8f5af
Nicholas Rodine <halfofastaple@gmail.com>
2024-02-28 04:27:02 -0500

    Moved Events tab to just the item editor.

--- DemonCastle.Editor/Editors/Item/ItemEditor.cs
+++ DemonCastle.Editor/Editors/Item/ItemEditor.cs
@@ -1,9 +1,14 @@
 using DemonCastle.Editor.Editors.Components.BaseEntity;
 using DemonCastle.ProjectFiles.Projects.Data;
 using DemonCastle.ProjectFiles.Projects.Resources;
+using Godot;
 
 namespace DemonCastle.Editor.Editors.Item;
 
 public partial class ItemEditor : BaseEntityEditor {
-	public ItemEditor(ProjectResources resources, ProjectInfo project, ItemInfo item) : base(resources, project, item, item, new ItemDetails(item)) { }
+	public ItemEditor(ProjectResources resources, ProjectInfo project, ItemInfo item) : base(resources, project, item,
+		item, new ItemDetails(item)) {
+
+		Tabs.AddTab("Events", new Label{Text = "Events"});
+	}
 }
