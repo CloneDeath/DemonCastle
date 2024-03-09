31ee94c85b6d8345f5c08fddbbe831b4f0ef2b56
Nicholas Rodine <halfofastaple@gmail.com>
2024-03-09 06:25:23 -0500

    Fixed bug where player would get stuck on stairs.

--- DemonCastle.Game/PlayerStates/ClimbingStairsState.cs
+++ DemonCastle.Game/PlayerStates/ClimbingStairsState.cs
@@ -8,6 +8,7 @@ public class ClimbingStairsState : IState {
 
 	public ClimbingStairsState(Tiles.GameTileStairs stairs) {
 		_stairs = stairs;
+		GD.Print("New Stairs Target: " + _stairs.GlobalPosition);
 	}
 
 	public void OnEnter(GamePlayer player) {
@@ -16,16 +17,18 @@ public class ClimbingStairsState : IState {
 	}
 
 	public IState? Update(GamePlayer player, double delta) {
-		return Input.IsActionPressed(InputActions.MoveUp) ? MoveTowards(player, UpTarget)
-			   : Input.IsActionPressed(InputActions.MoveDown) ? MoveTowards(player, DownTarget)
-			   : Input.IsActionPressed(InputActions.MoveLeft) ? MoveTowards(player, LeftTarget)
-			   : Input.IsActionPressed(InputActions.MoveRight) ? MoveTowards(player, RightTarget)
+		return Input.IsActionPressed(InputActions.MoveUp) ? MoveTowards(player, UpTarget, player.GlobalPosition.Y < UpTarget.GlobalPosition.Y)
+			   : Input.IsActionPressed(InputActions.MoveDown) ? MoveTowards(player, DownTarget, player.GlobalPosition.Y > DownTarget.GlobalPosition.Y)
+			   : Input.IsActionPressed(InputActions.MoveLeft) ? MoveTowards(player, LeftTarget, player.GlobalPosition.X < LeftTarget.GlobalPosition.X)
+			   : Input.IsActionPressed(InputActions.MoveRight) ? MoveTowards(player, RightTarget, player.GlobalPosition.X > RightTarget.GlobalPosition.X)
 			   : null;
 	}
 
-	private IState? MoveTowards(GamePlayer player, Node2D target) {
-		player.MoveTowards(target.GlobalPosition);
-		if (player.GlobalPosition.DistanceTo(target.GlobalPosition) > 0.5) return null;
+	private IState? MoveTowards(GamePlayer player, Node2D target, bool isBeyondTarget) {
+		if (!isBeyondTarget) {
+			player.MoveTowards(target.GlobalPosition);
+			if (player.GlobalPosition.DistanceTo(target.GlobalPosition) > 0.5) return null;
+		}
 
 		var stairs = player.GetNearbyStairs().FirstOrDefault(s => s != _stairs);
 		return stairs == null ? new NormalState() : new ClimbingStairsState(stairs);
