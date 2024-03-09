38283ac57158db5ea5af8e84e0ad66270af725a5
Nicholas Rodine <halfofastaple@gmail.com>
2024-03-09 08:10:03 -0500

    Started work on Events editor.

--- DemonCastle.Editor/Editors/Components/BaseEntity/BaseEntityTabContainer.cs
+++ DemonCastle.Editor/Editors/Components/BaseEntity/BaseEntityTabContainer.cs
@@ -1,4 +1,5 @@
 using DemonCastle.Editor.Editors.Components.Animations;
+using DemonCastle.Editor.Editors.Components.BaseEntity.Events;
 using DemonCastle.Editor.Editors.Components.States;
 using DemonCastle.Editor.Editors.Components.VariableDeclarations;
 using DemonCastle.ProjectFiles.Projects.Data;
@@ -11,6 +12,7 @@ public partial class BaseEntityTabContainer : TabContainer {
 	private readonly VariableCollectionEditor _variables;
 	private readonly AnimationsEditor _animations;
 	private readonly StatesEditor _states;
+	private readonly EventsEditor _events;
 
 	public BaseEntityTabContainer(ProjectResources resources, ProjectInfo project, IFileInfo file) {
 		Name = nameof(BaseEntityTabContainer);
@@ -18,6 +20,7 @@ public partial class BaseEntityTabContainer : TabContainer {
 		AddTab("Variables", _variables = new VariableCollectionEditor(resources));
 		AddTab("Animations", _animations = new AnimationsEditor(file));
 		AddTab("States", _states = new StatesEditor(resources, project));
+		AddTab("Events", _events = new EventsEditor());
 		CurrentTab = _animations.GetIndex();
 	}
 
@@ -30,5 +33,6 @@ public partial class BaseEntityTabContainer : TabContainer {
 		_variables.Load(entity?.Variables);
 		_animations.Load(entity?.Animations);
 		_states.Load(entity);
+		_events.Load(entity);
 	}
 }
---
+++ DemonCastle.Editor/Editors/Components/BaseEntity/Events/EventsEditor.cs
@@ -0,0 +1,14 @@
+using DemonCastle.ProjectFiles.Projects.Data;
+using Godot;
+
+namespace DemonCastle.Editor.Editors.Components.BaseEntity.Events;
+
+public partial class EventsEditor : Control {
+	public EventsEditor() {
+		Name = nameof(EventsEditor);
+	}
+
+	public void Load(IBaseEntityInfo? entity) {
+
+	}
+}
